ce7776fa77caf9f4c1ac9afdbb2ce6b452ae884c
zpate6 <zpate6@uic.edu>
2026-06-17 23:58:58 -0500

    update site logo

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -13,7 +13,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/logo.png" />
         <link rel="apple-touch-icon" href="/logo192.png" />
         <link rel="manifest" href="/manifest.json" />
       </head>
