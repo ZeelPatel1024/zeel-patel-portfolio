70a843a376a1f798c88bc6741ef0ebc9e200c5b3
zpate6 <zpate6@uic.edu>
2026-06-17 23:02:10 -0500

    Made it mobile responsive

--- src/sections/footer.jsx
+++ src/sections/footer.jsx
@@ -193,7 +193,7 @@ const Footer = () => {
               </div>
 
               <div className="social-icon-box">
-                <a href={myPDF} target="_blank" rel="noreferrer" className="social-icon-circle-link">
+                <a href="https://www.linkedin.com/in/zeel-patel2212/" target="_blank" rel="noreferrer" className="social-icon-circle-link">
                   <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="footer-social-icon-svg">
                     <path d="M10 13a5 5 0 0 0 7.54.54l3-3a5 5 0 0 0-7.07-7.07l-1.72 1.71" />
                     <path d="M14 11a5 5 0 0 0-7.54-.54l-3 3a5 5 0 0 0 7.07 7.07l1.71-1.71" />
